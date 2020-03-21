b4afff70a948ec47818d48e66f5f2de71625679b
liuzhanpeng <liuzhanpeng@gmail.com>
2020-03-21 20:38:23 +0800

    完成DbUserProvider

--- src/Auth.php
+++ src/Auth.php
@@ -7,6 +7,7 @@ use Lzpeng\Auth\Exception\ConfigException;
 use Lzpeng\Auth\Think\Authenticators\SessionAuthenticator;
 use Lzpeng\Auth\Think\Hashers\HasherInterface;
 use Lzpeng\Auth\Think\UserProviders\ModelUserProvider;
+use Lzpeng\Auth\UserProviders\DbUserProvider;
 use think\Container;
 
 class Auth extends AbstractAuth
@@ -17,6 +18,10 @@ class Auth extends AbstractAuth
     protected function init($authManager)
     {
         $authManager->registerUserProviderCreator('model', function ($config) {
+            if (!isset($config['model'])) {
+                throw new ConfigException('ModelUserProvider需要配置model');
+            }
+
             $modelClass = $config['model'];
             $idKey = $config['id_key'] ?? 'id';
             $passwordKey = $config['password_key'] ?? 'password';
@@ -28,6 +33,24 @@ class Auth extends AbstractAuth
             }
 
             return new ModelUserProvider($modelClass, $idKey, $passwordKey, $hasher);
+        });
+
+        $authManager->registerUserProviderCreator('db', function ($config) {
+            if (!isset($config['table'])) {
+                throw new ConfigException('DbUserProvider需要配置table');
+            }
+
+            $table = $config['table'];
+            $idKey = $config['id_key'] ?? 'id';
+            $passwordKey = $config['password_key'] ?? 'password';
+
+            // 因为hasher一般在其它场景(创建用户、修改密码等)时也会用到，所以直接获取注入容器内的hasher组件
+            $hasher = Container::get(HasherInterface::class);
+            if (is_null($hasher)) {
+                throw new ConfigException('注入容器中找不到实现HashInterfacer接口的hasher组件');
+            }
+
+            return new DbUserProvider($table, $idKey, $passwordKey, $hasher);
         });
 
         $authManager->registerAuthenticatorCreator('session', function ($config) {
--- src/UserProviders/DbUserProvider.php
+++ src/UserProviders/DbUserProvider.php
@@ -1,63 +1,67 @@
 <?php
 
-namespace Lzpeng\Auth\Think\UserProviders;
+namespace Lzpeng\Auth\UserProviders;
 
-use Lzpeng\Auth\Exception\InvalidCredentialException;
 use Lzpeng\Auth\Exception\Exception;
+use Lzpeng\Auth\Exception\InvalidCredentialException;
 use Lzpeng\Auth\Think\Hashers\HasherInterface;
 use Lzpeng\Auth\Think\PasswordInterface;
 use Lzpeng\Auth\UserInterface;
 use Lzpeng\Auth\UserProviderInterface;
+use Lzpeng\Auth\Users\GenericUser;
+use think\Db;
 
 /**
- * 基于think\Model的用户身份提供器
- * 通过用户标识/密码方式验证
+ * 基于think\Db的用户提供器
  * 
- * 注意: 密码是广义的, 除了用户密码，也可示表保存到模型的验证码、token等;
- * 
- * @author lzpeng <liuzhanpeng@gmail.com>
+ * @author 刘展鹏 <liuzhanpeng@gmail.com>
  */
-class ModelUserProvider implements UserProviderInterface
+class DbUserProvider implements UserProviderInterface
 {
     /**
-     * 模型类名
+     * 用户表
      *
      * @var string
      */
-    protected $modelClass;
+    private $table;
 
     /**
-     * 模型用户标识属性名称
+     * 用户表中的用户标识属性名称
      * 
      * @var string
      */
-    protected $idKey;
+    private $idKey;
 
     /**
      * 用户凭证数组里的密码key
      * 
      * @var string
      */
-    protected $passwordKey;
+    private $passwordKey;
 
     /**
-     * 哈希处理器
-     *
-     * @var HasherInterface
+     * hasher 
+     * 
+     * @var Hasher
      */
-    protected $hasher;
+    private $hasher;
 
     /**
      * 构造函数
-     *
-     * @param string $modelClass 模型类全称
-     * @param string $idKey 用于表示用户标识的属性的key
-     * @param string $passwordKey 用于表示密码的属性的key; 注意: 密码是广义的, 除了用户密码，也可示表保存到模型的验证码、token等;
-     * @param Lzpeng\Auth\Hasher\HasherInterface $hasher 哈希处理器
+     * 
+     * @param string $table 用户表
+     * @param string $idKey 模型id属性名称
+     * @param string $passwordKey 用户凭证数组里的密码key
+     * @param bool $forceValidatePassword 是否强制验证密码
+     * @param Hasher $hasher 密码hash处理器
      */
-    public function __construct(string $modelClass, string $idKey, string $passwordKey, HasherInterface $hasher)
-    {
-        $this->modelClass = $modelClass;
+    public function __construct(
+        string $table,
+        string $idKey = 'id',
+        string $passwordKey = 'password',
+        HasherInterface $hasher
+    ) {
+        $this->table = $table;
         $this->idKey = $idKey;
         $this->passwordKey = $passwordKey;
         $this->hasher = $hasher;
@@ -68,7 +72,13 @@ class ModelUserProvider implements UserProviderInterface
      */
     public function findById($id)
     {
-        return $this->createModel()->where($this->idKey, $id)->find();
+        $result = Db::name($this->table)->where($this->idKey, $id)->find();
+
+        if (!$result) {
+            return null;
+        }
+
+        return new GenericUser($result->toArray());
     }
 
     /**
@@ -80,8 +90,7 @@ class ModelUserProvider implements UserProviderInterface
             return null;
         }
 
-        $model = $this->createModel();
-        $query = $model::field('*');
+        $query = Db::name($this->table);
 
         // 循环设置查询条件
         foreach ($credentials as $key => $val) {
@@ -96,7 +105,12 @@ class ModelUserProvider implements UserProviderInterface
             }
         }
 
-        return $query->find();
+        $result =  $query->find();
+        if (!$result) {
+            return null;
+        }
+
+        return new GenericUser($result->toArray());
     }
 
     /**
@@ -115,19 +129,5 @@ class ModelUserProvider implements UserProviderInterface
         if (!$this->hasher->check($credentials[$this->passwordKey], $user->getPassword())) {
             throw new InvalidCredentialException('密码错误');
         }
-    }
-
-    /**
-     * 创建模型实例
-     */
-    private function createModel()
-    {
-        $class = '\\' . ltrim($this->modelClass, '\\');
-
-        if (!\class_exists($class)) {
-            throw new Exception(sprintf('找不到模型类[%s]', $class));
-        }
-
-        return new $class;
     }
 }
