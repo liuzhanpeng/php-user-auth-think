b35c3cec4e0fdb16c294e184fd99abcec615ea24
liuzhanpeng <liuzhanpeng@gmail.com>
2021-06-16 17:11:40 +0800

    fix

--- src/Auth.php
+++ src/Auth.php
@@ -32,7 +32,7 @@ class Auth extends AbstractAuth
             $passwordKey = $config['password_key'] ?? 'password';
             $hasherConfig = $config['hasher'] ?? ['driver' => BcryptHasher::class];
 
-            Container::set(HasherInterface::class, $hasherConfig['driver']);
+            Container::bind(HasherInterface::class, $hasherConfig['driver']);
             $hasher = Container::get(HasherInterface::class, $hasherConfig['params'] ?? []);
 
             return new ModelUserProvider($modelClass, $idKey, $passwordKey, $hasher);
@@ -48,7 +48,7 @@ class Auth extends AbstractAuth
             $passwordKey = $config['password_key'] ?? 'password';
             $hasherConfig = $config['hasher'] ?? ['driver' => BcryptHasher::class];
 
-            Container::set(HasherInterface::class, $hasherConfig['driver']);
+            Container::bind(HasherInterface::class, $hasherConfig['driver']);
             $hasher = Container::get(HasherInterface::class, $hasherConfig['params'] ?? []);
 
             return new DbUserProvider($table, $idKey, $passwordKey, $hasher);
